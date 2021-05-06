8c90cabd7dd4788c08d89cdaa3f2045d45341e43
bartikiel <64316791+bartikiel@users.noreply.github.com>
2021-05-06 20:16:04 +0200

    Changed stwitches in Classes to fix exit program

--- src/com/gitlab/filmica/AdminMenu.java
+++ src/com/gitlab/filmica/AdminMenu.java
@@ -5,63 +5,57 @@ import java.util.Scanner;
 
 
 
-public class AdminMenu extends MainMenu {
-
-}
-
-
-    public void runAdmin () {
+public class AdminMenu {
+    public static void run () {
         // Using Scanner to get input from user
         Scanner input = new Scanner(System.in);
 
         // User input
-
         int userInput = -1;
 
         //Admin menu
-        System.out.println("Menu: \n"+
-                "1- Dodaj film \n" +
-                "2- Usuń film \n" + //Here we need to add reservation option!
-                "3- Edytur repertuar \n" +
-                "4- Pokaż sprzedane bilety \n" +
-                "5- Wróć do menu usera \n" +
-                "6- Wyjdź z programu \n");
+        System.out.println("Menu: \n" +
+                "0- Wyjdź z programu \n" +
+                "1- Wróć do menu głównego \n" +
+                "2- Pokaż sprzedane bilety \n" +
+                "3- Dodaj film \n" +
+                "4- Usuń film \n" +
+                "5- Edytuj repertuar \n");
         System.out.println("Dokonaj wyboru z powyższych opcji: \n");
 
-
-
-        while (userInput! = 6){
+        while (userInput != 0){
             try{
                 userInput = input.nextInt();
 
                 switch (userInput) {
-                    case 1:
-                        System.out.println("Dodaję film");
-                        break;
-                    case 2:
-                        System.out.println("Usuwam film");
-                        break;
-                    case 3:
-                        System.out.println("Edytuję repertuar");
-                        break;
-                    case 4:
-                        System.out.println("Pokazuję sprzedane bilety");
-                        break;
-                    case 5:
-                        System.out.println("Menu usera");
-                        break;
-                    case 6:
+                    case 0:
                         System.out.println("Do widzenia!");
                         break;
+                    case 1:
+                        MainMenu.run();
+                        break;
+                    case 2:
+                        System.out.println("Pokazuję sprzedane bilety");
+                        break;
+                    case 3:
+                        System.out.println("Dodaję film");
+                        break;
+                    case 4:
+                        System.out.println("Usuwam film");
+                        break;
+                    case 5:
+                        System.out.println("Edytuję repertuar");
+                        break;
+
                     default:
-                    System.out.println("Wybór " + userInput + " jest niedostępny." + " Proszę wybrać jedną z poniższych opcji: \n" +
-                                    "Menu: \n"+
-                                    "1- Dodaj film \n" +
-                                    "2- Usuń film \n" +
-                                    "3- Edytur repertuar \n" +
-                                    "4- Pokaż sprzedane bilety \n" +
-                                    "5- Wróć do menu usera \n" +
-                                    "6- Wyjdź z programu \n");
+                        System.out.println("Wybór " + userInput + " jest niedostępny." + " Proszę wybrać jedną z poniższych opcji: \n" +
+                                "Menu: \n"+
+                                "1- Dodaj film \n" +
+                                "2- Usuń film \n" +
+                                "3- Edytur repertuar \n" +
+                                "4- Pokaż sprzedane bilety \n" +
+                                "5- Wróć do menu usera \n" +
+                                "6- Wyjdź z programu \n");
                 }
 
             }catch (Exception exception) {
@@ -70,5 +64,9 @@ public class AdminMenu extends MainMenu {
             }
         }
 
-    }
 }
+
+
+
+
+    }
--- src/com/gitlab/filmica/Main.java
+++ src/com/gitlab/filmica/Main.java
@@ -3,6 +3,7 @@ package com.gitlab.filmica;
 public class Main {
 
     public static void main(String[] args) {
-
+	// write your code here
+        MainMenu.run();
     }
 }
